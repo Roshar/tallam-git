54ea32fa5a9d9178ebd411076ef275d351803f9f
Roshar <rashid.batukaev@gmail.com>
2020-10-26 13:53:24 +0300

    version 1.5.1 delete sockets

--- models/school_admin/SchoolTeacher.js
+++ models/school_admin/SchoolTeacher.js
@@ -590,11 +590,9 @@ exports.updateTeacherAvatar  = async (req, res) =>{
    try {
       const dbh = await mysql.createConnection({
          host: process.env.DATABASE_HOST,
-         port: process.env.DATABASE_PORT,
          user: process.env.DATABASE_USER,
          database: process.env.DATABASE,
-         password: process.env.DATABASE_PASSWORD,
-         socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+         password: process.env.DATABASE_PASSWORD,  
       })
       
       const id_teacher = await req.teacher_id;
